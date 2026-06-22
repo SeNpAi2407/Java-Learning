884fa021fd9c092a12746347fdac5c2cb2fbc97a
SeNpAi2407 <darkpantera786@gmail.com>
2026-06-22 20:46:25 +0200

    first init

---
+++ OOP-2-main/Task 1/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    static String booksInfo = "";
+
+
+    public static void main(String[] args) {
+        while (true) {
+            String name = inputBookName();
+            int pageCount = inputPageCount();
+            double priceBook = bookPrice();
+            addBook(name, pageCount, priceBook);
+            printInfo();
+        }
+    }
+
+    public static String inputBookName() {
+        System.out.println("Введите название книги:");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static int inputPageCount() {
+        System.out.println("Введите кол-во страниц:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static int bookPrice() {
+        System.out.println("Введите цену:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void addBook(String bookName, int pageCount, double priceBook){
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр." + " - " + (priceBook > 0 ? priceBook + " евро" : "N/A");
+    }
+
+    public static void printInfo() {
+        System.out.println(booksInfo);
+    }
+
+}
